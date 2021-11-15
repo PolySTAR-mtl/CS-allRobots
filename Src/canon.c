57f22ce8666371329e93526c07f25acf2a1bf8d3
FreshOlives <75456969+FreshOlives@users.noreply.github.com>
2021-11-14 21:48:58 -0500

    Update canon.c

--- Src/canon.c
+++ Src/canon.c
@@ -6,65 +6,65 @@
                  
 #include "canon.h"
 
-#define TIME_BEFORE_START_FEEDER 10
-#define TIME_BEFORE_STOP 100
+#define FEEDER_START_DELAY 10
+#define SHUTDOWN_DELAY 100
 
 extern motor_t motors[MAX_MOTORS];
 extern refereeSystem_t refereeSystem;
-uint32_t begin_canon_shoot = 0; //0: Pas de s�quence de d�but de tir initialis� sinon temps du d�but de la s�quence
-uint32_t end_canon_shoot = 0; //0: Pas de s�quence de fin de tir initialis� sinon temps du d�but de la s�quence
-uint32_t shooting = 0; //0: Pas de s�quence de tir initialis� sinon temps du d�but de la s�quence
-float shoot_rate = 0; //Cadance de tir
-float shoot_rate2 = 0; //Cadance de tir pour le feeder #2 si il est present (ex. HEROS)
+uint32_t begin_canon_shoot = 0; // If 0, no firing start sequence initiated. Otherwise, timestamp of sequence start
+uint32_t end_canon_shoot = 0; 	// If 0, no firing end sequence initiated. Otherwise, timestamp of sequence start
+uint32_t shooting = 0; 			// If 0, no firing sequence initiated. Otherwise, timestamp of sequence start
+float shoot_rate = 0;  // Fire rate
+float shoot_rate2 = 0; // Fire rate for feeder #2, if it exists (ex. HEROS)
 
-//Demande de tirer
+// Initiate firing start sequence
 void canon_shoot_start(float speed, float rate){
 	
-	double coefficientVitesse = 1;
-	double coefficientCadence = 1;
+	double speed_multiplier = 1;
+	double rate_multiplier = 1;
 	/*
 	if((refereeSystem.shoot_data.bullet_speed / refereeSystem.game_robot_status.shooter_id2_17mm_speed_limit) > 0.90){
-		coefficientVitesse /= 1.2;
+		speed_multiplier /= 1.2;
 	} else if ((refereeSystem.shoot_data.bullet_speed / refereeSystem.game_robot_status.shooter_id2_17mm_speed_limit) < 0.70){
-		coefficientVitesse *= 1.2;
+		speed_multiplier *= 1.2;
 	}
-	speed *= coefficientVitesse;
+	speed *= speed_multiplier;
 	
 	if((refereeSystem.power_heat_data.shooter_id2_17mm_cooling_heat / refereeSystem.game_robot_status.shooter_id2_17mm_cooling_limit) > 0.90){
-		coefficientCadence = 0.5;
+		rate_multiplier = 0.5;
 	} else if ((refereeSystem.power_heat_data.shooter_id2_17mm_cooling_heat / refereeSystem.game_robot_status.shooter_id2_17mm_cooling_limit) < 0.70){
-		coefficientCadence = 1;
+		rate_multiplier = 1;
 	}
 	
-	rate *= coefficientCadence;
+	rate *= rate_multiplier;
 	*/
 	
-	// Si une des valeurs est null, on demande l'arret des tirs
+	// If either input is zero, initiate firing end sequence
 	if(speed == 0 || rate == 0){ 
 		canon_shoot_end();
 	}else{ 
-		//Si nous ne sommes pas entrain de tirer, nous d�butons la s�quence de tir
+		// If not currently firing or stopping, begin firing sequence
 		if(begin_canon_shoot == 0 && end_canon_shoot == 0 && shooting == 0){
 			begin_canon_shoot = HAL_GetTick();
 			
-			PWM_SetAllDuty(&htim1, speed, speed); //Demarage des snails
-			shoot_rate = motors[FEEDER].direction * rate; //Sauvegarde de la cadance de tir
+			PWM_SetAllDuty(&htim1, speed, speed); // Start up snails
+			shoot_rate = motors[FEEDER].direction * rate; // Store firing rate
 			
-	        motors[FEEDER].setpoint = shoot_rate; //Demarage du feeder
+	        motors[FEEDER].setpoint = shoot_rate; // Start up feeder
 			if (motors[FEEDER2].type == M2006) {
 				shoot_rate2 = motors[FEEDER2].direction * rate;
-			  motors[FEEDER2].setpoint = shoot_rate2; //Demarage du feeder #2 si il est present
+			  motors[FEEDER2].setpoint = shoot_rate2; // Start up feeder #2 if present
 		  }
 		}
 		
-		//Prise en compte des nouvelles valeurs que si nous sommes entrain de tirer
+		// If currently firing, update values
 		if(begin_canon_shoot == 0 && end_canon_shoot == 0 && shooting != 0){ 
-			PWM_SetAllDuty(&htim1, speed, speed); //Changement de la vitesse 
-			shoot_rate = motors[FEEDER].direction * rate; //Changement de la cadance de tir
-			motors[FEEDER].setpoint = shoot_rate; //Changement de la cadance de tir
+			PWM_SetAllDuty(&htim1, speed, speed); // Update speed
+			shoot_rate = motors[FEEDER].direction * rate; // Update local firing rate variable
+			motors[FEEDER].setpoint = shoot_rate; // Update firing rate in feeder motor struct
 			if (motors[FEEDER2].type == M2006) {
 				shoot_rate2 = motors[FEEDER2].direction * rate;
-			    motors[FEEDER2].setpoint = shoot_rate2; //Demarage du feeder #2 si il est present
+			    motors[FEEDER2].setpoint = shoot_rate2; // Update firing rate in feeder #2 motor struct, if present
 		  }
 		}
 	}
@@ -75,7 +75,7 @@ void canon_shoot_start(float speed, float rate){
 //Fonction de traitement des tirs, a appeler � chaque boucle
 void canon_process_inputs(){
 	//Si on a demand� de commencer de tirer il y a plus de x ms, on d�mare maintenant le feeder
-	if(begin_canon_shoot != 0 && HAL_GetTick() - begin_canon_shoot > TIME_BEFORE_START_FEEDER){
+	if(begin_canon_shoot != 0 && HAL_GetTick() - begin_canon_shoot > FEEDER_START_DELAY){
 		begin_canon_shoot = 0;
 		end_canon_shoot = 0;
 		shooting = HAL_GetTick();
@@ -87,7 +87,7 @@ void canon_process_inputs(){
 	}
 	
 	//Si on a demand� d'arreter de tirer il y a plus de x ms, on arrete tout
-	if(end_canon_shoot != 0 && HAL_GetTick() - end_canon_shoot > TIME_BEFORE_STOP){ 
+	if(end_canon_shoot != 0 && HAL_GetTick() - end_canon_shoot > SHUTDOWN_DELAY){ 
 		begin_canon_shoot = 0;
 		end_canon_shoot = 0;
 		shooting = 0;
